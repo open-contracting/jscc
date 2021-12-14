5a3edddefc886ccad12c496952d68ec71b302a4b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-12-14 13:21:07 -0500

    docs: Use furo

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
             'pytest-vcr',
         ],
         'docs': [
-            'Sphinx',
+            'furo',
+            'sphinx',
             'sphinx-autobuild',
-            'sphinx-rtd-theme',
         ],
     },
     classifiers=[
